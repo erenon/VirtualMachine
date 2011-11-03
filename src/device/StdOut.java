374a9630d3fcc97bc29defe231c7c135815323a0
erenon <erenon2@gmail.com>
2011-11-03 17:54:55 +0100

    StdOut output device tries to convert the given word to ascii char

--- src/device/StdOut.java
+++ src/device/StdOut.java
@@ -8,7 +8,24 @@ public class StdOut implements OutputDevice {
 			throw new AddressingException();
 		}
 		
-		System.out.print(word);
+		// convert integer stored in string to ascii char
+		int charCode = 0;
+		try {
+			charCode = Integer.parseInt(word);
+		} catch (NumberFormatException e) {
+			// word is not an integer's string representation
+			System.out.print(word);
+		}
+		
+		char c[] = null;
+		try {
+			c = Character.toChars(charCode);
+		} catch (IllegalArgumentException e) {
+			// can't convert to char array
+			System.out.print(word);
+		}
+		
+		System.out.print(c);
 	}
 
 }
