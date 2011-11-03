02f8bf2c847c7b88473115c0cc07544ffacc6a50
erenon <erenon2@gmail.com>
2011-11-03 17:52:37 +0100

    MemoryParameter added

--- src/cpu/instpar/MemoryParameter.java
+++ src/cpu/instpar/MemoryParameter.java
@@ -3,22 +3,36 @@ package cpu.instpar;
 import cpu.InstructionRunner;
 
 public class MemoryParameter implements InstructionParameter {
+	private String indirectAddress;
+	private int loadedValue;
+	
 	/**
-	 * @param memoryAddressExpression e.g: MEM[123]
+	 * @param memoryAddressExpression e.g: MEM[123], MEM[%eax], MEM[MEM[%ebx]]
 	 */
 	public MemoryParameter(String memoryAddressExpression) {
-		
+		int beginIndex = "MEM[".length();
+		// -1 because of the ending ]
+		int endIndex = memoryAddressExpression.length() - 1;
+		indirectAddress = memoryAddressExpression.substring(beginIndex, endIndex);
 	}
 
 	@Override
-	public void loadValue(InstructionRunner runner) {
-		// TODO Auto-generated method stub
+	public void loadValue(InstructionRunner runner) throws InvalidParameterException {
+		InstructionParameter addressParameter = runner.identifyInstructionParameter(indirectAddress);
+		addressParameter.loadValue(runner);
+		int address = addressParameter.getValue();
 		
+		// fetch value from address
+		String word = runner.fetchWord(address);
+		try {
+			loadedValue = Integer.parseInt(word);
+		} catch (NumberFormatException e) {
+			throw new InvalidParameterException();
+		}
 	}
 
 	@Override
 	public int getValue() {
-		// TODO Auto-generated method stub
-		return 0;
+		return loadedValue;
 	}
 }
