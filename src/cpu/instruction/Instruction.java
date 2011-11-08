0b38be72eb69e4e299f0c8b649212d7d890da0bc
erenon <erenon2@gmail.com>
2011-11-08 09:38:39 +0100

    Instructions have to load their own parameters

--- src/cpu/instruction/Instruction.java
+++ src/cpu/instruction/Instruction.java
@@ -2,10 +2,12 @@ package cpu.instruction;
 
 import cpu.InstructionRunner;
 import cpu.instpar.InstructionParameter;
+import cpu.instpar.InvalidParameterException;
 
 public interface Instruction {
 	public void execute(
 		InstructionRunner runner, 
 		InstructionParameter[] parameters
-	) throws InvalidInstructionException;
+	) throws InvalidInstructionException,
+			 InvalidParameterException;
 }
--- src/cpu/instruction/Out.java
+++ src/cpu/instruction/Out.java
@@ -2,19 +2,21 @@ package cpu.instruction;
 
 import cpu.InstructionRunner;
 import cpu.instpar.InstructionParameter;
+import cpu.instpar.InvalidParameterException;
 
 public class Out implements Instruction {
 
 	@Override
 	public void execute(InstructionRunner runner,
-		InstructionParameter[] parameters) throws InvalidInstructionException
+		InstructionParameter[] parameters) 
+		throws InvalidInstructionException, InvalidParameterException
 	{
 		if (parameters.length != 2) {
 			throw new InvalidInstructionException();
 		}
 		
-		int address = parameters[0].getValue();
-		String word = Integer.toString(parameters[1].getValue());
+		int address = parameters[0].loadValue(runner);
+		String word = Integer.toString(parameters[1].loadValue(runner));
 		
 		runner.putWord(address, word);
 	}
