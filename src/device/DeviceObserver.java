5ebd98083741acfcbe7aab3e9a168a8970a3d39a
erenon <erenon2@gmail.com>
2011-11-12 15:48:30 +0100

    VmDisplay added, uses a very rigid model concept

---
+++ src/device/DeviceObserver.java
@@ -0,0 +1,5 @@
+package device;
+
+public interface DeviceObserver {
+	public void fireDataChange(int address);
+}
--- src/device/VmDisplay.java
+++ src/device/VmDisplay.java
@@ -1,5 +1,42 @@
 package device;
 
-public class VmDisplay implements Device {
+import ui.model.DisplayModel;
+
+public class VmDisplay implements OutputDevice {
+	DisplayModel model;
+	
+	public VmDisplay(DisplayModel model) {
+		this.model = model;
+	}
+	
+	@Override
+	public void putWord(int address, String word) throws AddressingException {
+		if (address != 0) {
+			throw new AddressingException();
+		}
+		
+		// convert integer stored in string to ascii char
+		int charCode = 0;
+		try {
+			charCode = Integer.parseInt(word);
+		} catch (NumberFormatException e) {
+			// word is not an integer's string representation
+			e.printStackTrace();
+			model.putWord(word);
+		}
+		
+		char c[] = null;
+		try {
+			c = Character.toChars(charCode);
+		} catch (IllegalArgumentException e) {
+			// can't convert to char array
+			e.printStackTrace();
+			model.putWord(word);
+		}
+		
+		String out = new String(c);
+		
+		model.putWord(out);
+	}
 
 }
